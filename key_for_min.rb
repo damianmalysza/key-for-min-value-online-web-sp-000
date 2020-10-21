b944efa0497c5071d8c10e9e83666fa3399d3846
Damian Malysza <damian.malysza@gmail.com>
2020-10-21 23:04:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,8 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   else
-
+    name_hash.each do |key,value|
+      if value < smallest_value
+        smallest_key = key
   end
 end
